85c755886fd7ee1adffd5f02139f170e9d32a549
Diana <izabellaslivina@gmail.com>
2024-02-18 01:10:47 +0300

    Добавьте файлы проекта.

---
+++ Plus3.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	setlocale(LC_ALL, "Russian");
+
+	double myarray[3][5];
+	double midA = 0, midB = 0, midC = 0, mid = 0;
+
+	cout << "Введите массив построчно\n";
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 5; j++) {
+			cin >> myarray[i][j];
+			mid += myarray[i][j];
+		}
+		cout << endl;
+		switch (i)
+		{
+			case 0:
+				midA = mid / 5;
+				break;
+			case 1:
+				midB = mid / 5;
+				break;
+			case 2:
+				midC = mid / 5;
+				break;
+		}
+		mid = 0;
+	}
+	cout << "Cреднее значение строк:\n" << midA << " " << midB << " " << midC;
+
+	return 0;
+}
